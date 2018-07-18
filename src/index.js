c887908a2b87fee57cb547a7338f789acc508387
Sam Worrall <samuel.worrall@hotmail.co.uk>
2018-07-18 15:09:59 +0100

    Add interactive history of game

--- src/index.js
+++ src/index.js
@@ -52,12 +52,13 @@ class Game extends React.Component {
     super(props);
     this.state = {
       history: [{ squares: Array(9).fill(null) }],
+      stepNumber: 0,
       turn: true,
     }
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
     const current = history[history.length - 1];
     const newSquares = current.squares.slice();
     const newTurn = !this.state.turn
@@ -69,14 +70,31 @@ class Game extends React.Component {
       history: history.concat([{
         squares: newSquares,
       }]),
+      stepNumber: history.length,
       turn: newTurn,
     })
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      turn: (step % 2) === 0,
+    });
+  }
+
   render() {
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move) => {
+      const desc = move ? 'Go to move #' + move : 'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
 
     let status;
     if (winner) {
@@ -95,7 +113,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
